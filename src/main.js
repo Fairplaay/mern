ca5d6ae088c920e910808e306cfad957ce3f82fa
Jesus Garcia <fairplaay@gmail.com>
2018-04-15 22:50:10 -0400

    agregando react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('hello universe abril 2018')
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+class MyApp extends Component {
+  render () {
+    return (
+      <div>Hello universe</div>
+    )
+  }
+}
+
+render(<MyApp />, document.getElementById('myApp'))
